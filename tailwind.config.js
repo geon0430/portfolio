eb1daec9dd1f397c273dcf4e2a93b52f9386bbb0
geon <changgeon.lee@inbic.ai>
2024-02-21 15:10:48 +0900

    color

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,12 +18,12 @@ module.exports = {
         /**
          * @description selection(드래그 블록)에 사용되는 컬러입니다.
          */
-        PRIMARY_LIGHT: "#c3fae8",
+        PRIMARY_LIGHT: "#3ec5f1",
 
         /**
          * @description 자기소개의 이름 부분, code tag의 darkmode에 사용되는 컬러입니다.
          */
-        PRIMARY: "#12b886",
+        PRIMARY: "#000080",
 
         /**
          * @description code tag, link hover icon에 사용되는 컬러입니다.
@@ -33,12 +33,12 @@ module.exports = {
         /**
          * @description 페이지 최상단 gradient의 시작 컬러입니다.
          */
-        GRADIENT_FROM: "#51cf66",
+        GRADIENT_FROM: "#00498c",
 
         /**
          * @description 페이지 최상단 gradient의 종료 컬러입니다.
          */
-        GRADIENT_TO: "#0c8599",
+        GRADIENT_TO: "#ffffff",
       },
     },
   },
